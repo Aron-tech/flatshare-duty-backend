d22132efef5ac14af731135f0029e5b389a7593c
Aron <aron.papp2003@gmail.com>
2026-09-25 21:32:44 +0200

    fix getfullname

--- app/Models/User.php
+++ app/Models/User.php
@@ -31,7 +31,7 @@ class User extends Authenticatable
 
     public function getFullName(): string
     {
-        return LanguageEnum::from($this->language) === LanguageEnum::HUNGARIAN ? "{$this->last_name} {$this->first_name}" : "{$this->first_name} {$this->last_name}";
+        return $this->language === LanguageEnum::HUNGARIAN ? "{$this->last_name} {$this->first_name}" : "{$this->first_name} {$this->last_name}";
     }
 
     public function households(): BelongsToMany
